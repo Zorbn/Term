2355c23e7a920b1b18c28fa2adffef0d942f7eb6
Zorbn <public.zorb@gmail.com>
2023-09-03 22:42:17 -0700

    Parse some positioning sequences

--- src/main.c
+++ src/main.c
@@ -25,133 +25,152 @@ const int32_t grid_width = 80;
 const int32_t grid_height = 24;
 
 typedef struct {
-	HRESULT result;
-	HANDLE output, input;
+    HRESULT result;
+    HANDLE output, input;
 } PseudoConsole;
 
 typedef struct {
-	wchar_t *text;
-	UINT32 textLength;
-	RECT rc;
+    wchar_t *text;
+    UINT32 textLength;
+    RECT rc;
 } Data;
 
+bool data_match_char(Data *data, wchar_t character, size_t *i) {
+    if (*i >= data->textLength) {
+        return false;
+    }
+
+    if (data->text[*i] != character) {
+        return false;
+    }
+
+    *i += 1;
+    return true;
+}
+
+bool data_peek_digit(Data *data, size_t i) {
+    if (i >= data->textLength) {
+        return false;
+    }
+
+    return iswdigit(data->text[i]);
+}
+
+void data_destroy(Data *data) {
+    free(data->text);
+}
+
 HRESULT PrepareStartupInformation(HPCON hpc, STARTUPINFOEX *psi) {
-	// Prepare Startup Information structure
-	STARTUPINFOEX si;
-	ZeroMemory(&si, sizeof(si));
-	si.StartupInfo.cb = sizeof(STARTUPINFOEX);
+    // Prepare Startup Information structure
+    STARTUPINFOEX si;
+    ZeroMemory(&si, sizeof(si));
+    si.StartupInfo.cb = sizeof(STARTUPINFOEX);
 
-	// Discover the size required for the list
-	size_t bytesRequired;
-	InitializeProcThreadAttributeList(NULL, 1, 0, &bytesRequired);
+    // Discover the size required for the list
+    size_t bytesRequired;
+    InitializeProcThreadAttributeList(NULL, 1, 0, &bytesRequired);
 
-	// Allocate memory to represent the list
-	si.lpAttributeList = (PPROC_THREAD_ATTRIBUTE_LIST)HeapAlloc(
-		GetProcessHeap(), 0, bytesRequired);
-	if (!si.lpAttributeList) {
-		return E_OUTOFMEMORY;
-	}
+    // Allocate memory to represent the list
+    si.lpAttributeList = (PPROC_THREAD_ATTRIBUTE_LIST)HeapAlloc(GetProcessHeap(), 0, bytesRequired);
+    if (!si.lpAttributeList) {
+        return E_OUTOFMEMORY;
+    }
 
-	// Initialize the list memory location
-	if (!InitializeProcThreadAttributeList(si.lpAttributeList, 1, 0,
-										   &bytesRequired)) {
-		HeapFree(GetProcessHeap(), 0, si.lpAttributeList);
-		return HRESULT_FROM_WIN32(GetLastError());
-	}
+    // Initialize the list memory location
+    if (!InitializeProcThreadAttributeList(si.lpAttributeList, 1, 0, &bytesRequired)) {
+        HeapFree(GetProcessHeap(), 0, si.lpAttributeList);
+        return HRESULT_FROM_WIN32(GetLastError());
+    }
 
-	// Set the pseudoconsole information into the list
-	if (!UpdateProcThreadAttribute(si.lpAttributeList, 0,
-								   PROC_THREAD_ATTRIBUTE_PSEUDOCONSOLE, hpc,
-								   sizeof(hpc), NULL, NULL)) {
-		HeapFree(GetProcessHeap(), 0, si.lpAttributeList);
-		return HRESULT_FROM_WIN32(GetLastError());
-	}
+    // Set the pseudoconsole information into the list
+    if (!UpdateProcThreadAttribute(
+            si.lpAttributeList, 0, PROC_THREAD_ATTRIBUTE_PSEUDOCONSOLE, hpc, sizeof(hpc), NULL, NULL)) {
+        HeapFree(GetProcessHeap(), 0, si.lpAttributeList);
+        return HRESULT_FROM_WIN32(GetLastError());
+    }
 
-	*psi = si;
+    *psi = si;
 
-	return S_OK;
+    return S_OK;
 }
 
 PseudoConsole SetUpPseudoConsole(COORD size) {
-	HRESULT hr = S_OK;
+    HRESULT hr = S_OK;
 
-	// - Close these after CreateProcess of child application with
-	// pseudoconsole object.
-	HANDLE inputReadSide, outputWriteSide;
+    // - Close these after CreateProcess of child application with
+    // pseudoconsole object.
+    HANDLE inputReadSide, outputWriteSide;
 
-	// - Hold onto these and use them for communication with the child
-	// through the pseudoconsole.
-	HANDLE outputReadSide, inputWriteSide;
+    // - Hold onto these and use them for communication with the child
+    // through the pseudoconsole.
+    HANDLE outputReadSide, inputWriteSide;
 
-	if (!CreatePipe(&inputReadSide, &inputWriteSide, NULL, 0)) {
-		return (PseudoConsole){
-			HRESULT_FROM_WIN32(GetLastError()),
-			NULL,
-			NULL,
-		};
-	}
+    if (!CreatePipe(&inputReadSide, &inputWriteSide, NULL, 0)) {
+        return (PseudoConsole){
+            HRESULT_FROM_WIN32(GetLastError()),
+            NULL,
+            NULL,
+        };
+    }
 
-	if (!CreatePipe(&outputReadSide, &outputWriteSide, NULL, 0)) {
-		return (PseudoConsole){
-			HRESULT_FROM_WIN32(GetLastError()),
-			NULL,
-			NULL,
-		};
-	}
+    if (!CreatePipe(&outputReadSide, &outputWriteSide, NULL, 0)) {
+        return (PseudoConsole){
+            HRESULT_FROM_WIN32(GetLastError()),
+            NULL,
+            NULL,
+        };
+    }
 
-	HPCON hPC;
-	hr = CreatePseudoConsole(size, inputReadSide, outputWriteSide, 0, &hPC);
-	if (FAILED(hr)) {
-		return (PseudoConsole){
-			hr,
-			NULL,
-			NULL,
-		};
-	}
+    HPCON hPC;
+    hr = CreatePseudoConsole(size, inputReadSide, outputWriteSide, 0, &hPC);
+    if (FAILED(hr)) {
+        return (PseudoConsole){
+            hr,
+            NULL,
+            NULL,
+        };
+    }
 
-	STARTUPINFOEX siEx;
-	PrepareStartupInformation(hPC, &siEx);
+    STARTUPINFOEX siEx;
+    PrepareStartupInformation(hPC, &siEx);
 
-	// PCWSTR childApplication = L"C:\\windows\\system32\\cmd.exe";
-	// PCWSTR childApplication = L"C:\\Program Files\\PowerShell\\7\\pwsh.exe";
-	PCWSTR childApplication = L"python test.py";
+    // PCWSTR childApplication = L"C:\\windows\\system32\\cmd.exe";
+    // PCWSTR childApplication = L"C:\\Program Files\\PowerShell\\7\\pwsh.exe";
+    PCWSTR childApplication = L"python test.py";
 
-	// Create mutable text string for CreateProcessW command line string.
-	const size_t charsRequired =
-		wcslen(childApplication) + 1; // +1 null terminator
-	PWSTR cmdLineMutable =
-		(PWSTR)HeapAlloc(GetProcessHeap(), 0, sizeof(wchar_t) * charsRequired);
+    // Create mutable text string for CreateProcessW command line string.
+    const size_t charsRequired = wcslen(childApplication) + 1; // +1 null terminator
+    PWSTR cmdLineMutable = (PWSTR)HeapAlloc(GetProcessHeap(), 0, sizeof(wchar_t) * charsRequired);
 
-	if (!cmdLineMutable) {
-		return (PseudoConsole){
-			E_OUTOFMEMORY,
-			NULL,
-			NULL,
-		};
-	}
+    if (!cmdLineMutable) {
+        return (PseudoConsole){
+            E_OUTOFMEMORY,
+            NULL,
+            NULL,
+        };
+    }
 
-	wcscpy_s(cmdLineMutable, charsRequired, childApplication);
+    wcscpy_s(cmdLineMutable, charsRequired, childApplication);
 
-	PROCESS_INFORMATION pi;
-	ZeroMemory(&pi, sizeof(pi));
+    PROCESS_INFORMATION pi;
+    ZeroMemory(&pi, sizeof(pi));
 
-	// Call CreateProcess
-	if (!CreateProcessW(NULL, cmdLineMutable, NULL, NULL, FALSE,
-						EXTENDED_STARTUPINFO_PRESENT, NULL, NULL,
-						(LPSTARTUPINFOW)(&siEx.StartupInfo), &pi)) {
-		HeapFree(GetProcessHeap(), 0, cmdLineMutable);
-		return (PseudoConsole){
-			HRESULT_FROM_WIN32(GetLastError()),
-			NULL,
-			NULL,
-		};
-	}
+    // Call CreateProcess
+    if (!CreateProcessW(NULL, cmdLineMutable, NULL, NULL, FALSE, EXTENDED_STARTUPINFO_PRESENT, NULL, NULL,
+            (LPSTARTUPINFOW)(&siEx.StartupInfo), &pi)) {
+        HeapFree(GetProcessHeap(), 0, cmdLineMutable);
+        return (PseudoConsole){
+            HRESULT_FROM_WIN32(GetLastError()),
+            NULL,
+            NULL,
+        };
+    }
 
-	return (PseudoConsole){
-		hr,
-		outputReadSide,
-		inputWriteSide,
-	};
+    return (PseudoConsole){
+        hr,
+        outputReadSide,
+        inputWriteSide,
+    };
 }
 
 struct Grid {
@@ -183,8 +202,9 @@ void grid_destroy(struct Grid *grid) {
     free(grid->data);
 }
 
-void grid_move_cursor(struct Grid *grid, int32_t delta_x, int32_t delta_y) {
-    grid->cursor_x += delta_x;
+void grid_cursor_move_to(struct Grid *grid, int32_t x, int32_t y) {
+    grid->cursor_x = x;
+    grid->cursor_y = y;
 
     if (grid->cursor_x < 0) {
         // TODO: Is this right, or should it loop around?
@@ -192,15 +212,126 @@ void grid_move_cursor(struct Grid *grid, int32_t delta_x, int32_t delta_y) {
     }
 
     // TODO: This is a hack, we are tying the visible cursor position and the actual cursor position together.
-    // That will cause problems when implementing resizing, scrollback, etc. Maybe replace this with proper line wrap later.
+    // That will cause problems when implementing resizing, scrollback, etc. Maybe replace this with proper line wrap
+    // later.
     while (grid->cursor_x >= grid->width) {
         grid->cursor_x -= grid->width;
         grid->cursor_y++;
     }
 
-    grid->cursor_y += delta_y;
-
     // TODO: Handle scrolling.
+}
+
+void grid_cursor_move(struct Grid *grid, int32_t delta_x, int32_t delta_y) {
+    grid_cursor_move_to(grid, grid->cursor_x + delta_x, grid->cursor_y + delta_y);
+}
+
+// Returns true if an escape sequence was parsed.
+bool grid_parse_escape_sequence(struct Grid *grid, Data *data, size_t *i) {
+    size_t start_i = *i;
+    if (!data_match_char(data, L'\x1b', i)) {
+        *i = start_i;
+        return false;
+    }
+
+    if (data_match_char(data, L'[', i)) {
+        // TODO: Make sure when parsing sequences that they don't have more numbers supplied then they allow, ie: no
+        // ESC[10;5A because A should only accept one number.
+        size_t parsed_number_count = 0;
+        int32_t number1 = 0;
+        int32_t number2 = 0;
+
+        // TODO: Make this into a function.
+        while (data_peek_digit(data, *i)) {
+            parsed_number_count = 1;
+            int32_t digit = data->text[*i] - L'0';
+            number1 = number1 * 10 + digit;
+            *i += 1;
+        }
+
+        if (data_match_char(data, L';', i)) {
+            while (data_peek_digit(data, *i)) {
+                parsed_number_count = 2;
+                int32_t digit = data->text[*i] - L'0';
+                number2 = number2 * 10 + digit;
+                *i += 1;
+            }
+        }
+
+        // Text formatting:
+        {
+            if (data_match_char(data, L'm', i)) {
+                // TODO: Change format.
+                return true;
+            }
+        }
+
+        // Cursor positioning:
+        {
+            if (parsed_number_count < 2) {
+                int32_t n = parsed_number_count > 0 ? number1 : 1;
+
+                // Up:
+                if (data_match_char(data, L'A', i)) {
+                    grid_cursor_move(grid, 0, -n);
+                    return true;
+                }
+
+                // Down:
+                if (data_match_char(data, L'B', i)) {
+                    grid_cursor_move(grid, 0, n);
+                    return true;
+                }
+
+                // Backward:
+                if (data_match_char(data, L'D', i)) {
+                    grid_cursor_move(grid, -n, 0);
+                    return true;
+                }
+
+                // Forward:
+                if (data_match_char(data, L'C', i)) {
+                    grid_cursor_move(grid, n, 0);
+                    return true;
+                }
+            }
+
+            int32_t x = parsed_number_count > 0 ? number1 : 1;
+            int32_t y = parsed_number_count > 1 ? number2 : 1;
+
+            // Forward:
+            if (data_match_char(data, L'H', i)) {
+                grid_cursor_move_to(grid, x - 1, y - 1);
+                return true;
+            }
+        }
+
+        // Text modification:
+        {
+            int32_t n = parsed_number_count > 0 ? number1 : 0;
+
+            switch (n) {
+                case 2: {
+                    if (data_match_char(data, L'J', i)) {
+                        // Earase entire display.
+                        for (size_t y = 0; y < grid->height; y++) {
+                            for (size_t x = 0; x < grid->width; x++) {
+                                grid->data[x + y * grid->width] = L' ';
+                            }
+                        }
+
+                        return true;
+                    }
+
+                    break;
+                }
+            }
+        }
+    }
+
+    // This sequence is invalid, ignore it.
+    *i = start_i;
+    return false;
 }
 
 void draw_character(struct SpriteBatch *sprite_batch, wchar_t character, int32_t x, int32_t y, int32_t origin_y) {
@@ -209,14 +340,14 @@ void draw_character(struct SpriteBatch *sprite_batch, wchar_t character, int32_t
     }
 
     sprite_batch_add(sprite_batch, (struct Sprite){
-                                        .x = x * 6,
-                                        .y = origin_y - (y + 1) * 14,
-                                        .width = 6,
-                                        .height = 14,
-                                        .texture_x = 8 * (character - 32),
-                                        .texture_width = 6,
-                                        .texture_height = 14,
-                                    });
+                                       .x = x * 6,
+                                       .y = origin_y - (y + 1) * 14,
+                                       .width = 6,
+                                       .height = 14,
+                                       .texture_x = 8 * (character - 32),
+                                       .texture_width = 6,
+                                       .texture_height = 14,
+                                   });
 }
 
 int main() {
@@ -291,49 +422,23 @@ int main() {
                 data.textLength = dwRead + 1;
 
                 for (size_t i = 0; i < data.textLength;) {
-                    // Check for escape sequences. https://learn.microsoft.com/en-us/windows/console/console-virtual-terminal-sequences
-                    // If <n> is omitted for colors, it is assumed to be 0, if <x,y,n> are omitted for positioning, they are assumed to be 1.
-                    size_t start_i = i;
-                    if (data.text[i] == L'\x1b') {
-                        i++;
-
-                        if (data.text[i] == L'[') {
-                            i++;
-                            if (data.text[i] == L'm') {
-                                i++;
-                                // TODO: Reset colors.
-                                continue;
-                            }
-
-                            while (iswdigit(data.text[i])) {
-                                i++;
-                            }
-
-                            if (data.text[i] == L'm') {
-                                i++;
-                                // TODO: Change colors.
-                                continue;
-                            }
-
-                            if (data.text[i] == L'C') {
-                                i++;
-                                // TODO: Move by the parsed number.
-                                grid_move_cursor(&grid, 1, 0);
-                                continue;
-                            }
-                        }
-
-                        // This sequence is invalid, ignore it.
-                        i = start_i;
+                    // Check for escape sequences.
+                    // https://learn.microsoft.com/en-us/windows/console/console-virtual-terminal-sequences If <n> is
+                    // omitted for colors, it is assumed to be 0, if <x,y,n> are omitted for positioning, they are
+                    // assumed to be 1.
+                    if (grid_parse_escape_sequence(&grid, &data, &i)) {
+                        continue;
                     }
 
                     if (data.text[i] == L'\n') {
                         grid.cursor_x = 0;
                         grid.cursor_y++;
+                        i++;
+                        continue;
                     }
 
                     grid.data[grid.cursor_x + grid.cursor_y * grid_width] = data.text[i];
-                    grid_move_cursor(&grid, 1, 0);
+                    grid_cursor_move(&grid, 1, 0);
 
                     i++;
                 }
@@ -367,6 +472,7 @@ int main() {
     }
 
     grid_destroy(&grid);
+    data_destroy(&data);
 
     sprite_batch_destroy(&sprite_batch);
 
